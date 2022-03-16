fabc363d1a463d05685c880dd97b4645272ce379
Thuan Van. Vo <votuonganvn@gmail.com>
2022-03-16 21:28:53 +0700

    Update: User can choose the specific year to parse or the whole data

--- getAlertList_v2.py
+++ getAlertList_v2.py
@@ -4,26 +4,17 @@ import xml.etree.ElementTree as Xet
 import pandas as pd
 from xml.etree import ElementTree
 import csv
+import datetime
 
-# Get XML from API
-response = requests.get("https://jvndb.jvn.jp/myjvn?method=getAlertList&feed=hnd")
-root = ET.fromstring(response.text)
-tree = ET.ElementTree(root)
-tree.write("data_alert.xml")
-print('Get API to XML successfully')
+yearToParse = input("Please input 'all' to parse the whole data or any specific year: ")
+today = datetime.date.today()
+if(yearToParse == 'all'):
+    startYear = 2018
+    endYear = today.year + 1
+else:
+    startYear = int(yearToParse)
+    endYear = int(yearToParse) + 1
 
-fixedFile = open("data_alert_fixed.xml",'w', encoding='utf8')
-with open('data_alert.xml') as file:
-    for line in file:
-            line = line.replace("ns0:", "")
-            line = line.replace("ns1:", "")
-            line = line.replace("ns2:", "")
-            line = line.replace("ns3:", "")
-            line = line.replace("ns4:", "")
-            line = line.replace("ns5:", "")
-            fixedFile.write(line)
-
-fixedFile.close()
 # CREATE CSV FILE
 csvfile_1 = open("alert_list.csv",'w', encoding='utf8', newline="")
 csvfile_writer_1 = csv.writer(csvfile_1)
@@ -31,29 +22,50 @@ csvfile_writer_1 = csv.writer(csvfile_1)
 # ADD THE HEADER TO CSV FILE
 csvfile_writer_1.writerow(["title", "id", "title-item","id-item", "link-item", "cpe-item", "published-item", "updated-item"])
 
-# Parsing XML to CSV
-xml = ElementTree.parse("data_alert_fixed.xml")
-root = xml.getroot()
+# Get XML from API
+for year in range (startYear, endYear):
+    pathApi = "https://jvndb.jvn.jp/myjvn?method=getAlertList&feed=hnd&datePublished=" + str(year)
+    response = requests.get(pathApi)
+    root = ET.fromstring(response.text)
+    tree = ET.ElementTree(root)
+    tree.write("data_alert.xml")
+    print('Get API to XML in ' + str(year) + ' successfully')
 
-for object in root.findall("./entry"):
-    currentTitle = object.find("title").text
-    currentId = object.find("id").text
-    
-    for item in object.findall("./items/item"):
-        currentTitleItem = item.find("title").text
-        currentIdItem = item.find("identifier").text
-        currentLinkItem = item.find("link").attrib['href']
-        print(str(item.find("cpe")))
-        if(str(item.find("cpe")) != 'None'):
-            currentCpeItem = item.find("cpe").text
-        else: 
-            currentCpeItem = "-"
+    fixedFile = open("data_alert_fixed.xml",'w', encoding='utf8')
+    with open('data_alert.xml') as file:
+        for line in file:
+                line = line.replace("ns0:", "")
+                line = line.replace("ns1:", "")
+                line = line.replace("ns2:", "")
+                line = line.replace("ns3:", "")
+                line = line.replace("ns4:", "")
+                line = line.replace("ns5:", "")
+                fixedFile.write(line)
 
-        currentPublishedItem = item.find("published").text
-        currentUpdatedItem = item.find("updated").text
+    fixedFile.close()
 
-        currentCsvLine = [currentTitle, currentId, currentTitleItem, currentIdItem, currentLinkItem, currentCpeItem, currentPublishedItem, currentUpdatedItem]
-        csvfile_writer_1.writerow(currentCsvLine)
+    # Parsing XML to CSV
+    xml = ElementTree.parse("data_alert_fixed.xml")
+    root = xml.getroot()
 
-print('csv parsed successfully')
+    for object in root.findall("./entry"):
+        currentTitle = object.find("title").text
+        currentId = object.find("id").text
+        
+        for item in object.findall("./items/item"):
+            currentTitleItem = item.find("title").text
+            currentIdItem = item.find("identifier").text
+            currentLinkItem = item.find("link").attrib['href']
+            if(str(item.find("cpe")) != 'None'):
+                currentCpeItem = item.find("cpe").text
+            else: 
+                currentCpeItem = "-"
+
+            currentPublishedItem = item.find("published").text
+            currentUpdatedItem = item.find("updated").text
+
+            currentCsvLine = [currentTitle, currentId, currentTitleItem, currentIdItem, currentLinkItem, currentCpeItem, currentPublishedItem, currentUpdatedItem]
+            csvfile_writer_1.writerow(currentCsvLine)
+
+    print('csv parsed in ' + str(year) + ' successfully')
 
